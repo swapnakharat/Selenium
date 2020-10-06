f21b284aaba771986f48af43e2d41d3cfc989c71
swapna kharat <swapnakharat18@gmail.com>
2020-10-06 16:52:42 +0530

    "First commit"

---
+++ src/test/java/Practice.java
@@ -0,0 +1,16 @@
+public class Practice {
+
+	public static void main(String[] args) 
+	{
+		String hungry="yes";
+		if(hungry== "yes")
+		{
+			System.out.println("eat something");
+		}
+		else
+		{
+			System.out.println("do work");
+		}
+	}
+
+}
